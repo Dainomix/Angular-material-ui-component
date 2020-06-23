bf8bace5400c073727a8b899212771d09dbe5327
Kim Hyeoncheol <effectmix@gmail.com>
2020-06-23 23:55:23 +0900

    Buttons in Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from "@angular/material/button";
 import { MatSliderModule } from '@angular/material/slider';
+
 import { TypographyComponent } from './typography/typography.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
