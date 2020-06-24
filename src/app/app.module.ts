c1da349d86f18c0ce0182f3c99127f7b2b45a4d9
Kim Hyeoncheol <effectmix@gmail.com>
2020-06-24 22:35:12 +0900

    Togglebutton in Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from "@angular/material/button";
 import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 
 import { TypographyComponent } from './typography/typography.component';
 import { ButtonComponent } from './button/button.component';
+import { TogglebuttonComponent } from './togglebutton/togglebutton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
     ButtonComponent,
+    TogglebuttonComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +26,7 @@ import { ButtonComponent } from './button/button.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
+    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
